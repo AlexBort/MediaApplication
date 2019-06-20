cfb6f99cdd6b9511330a78b67f4091a3cfd699f5
AlexBort <bortolo1994@gmail.com>
2019-06-20 20:25:27 +0300

    show list of imageContent

--- app/src/main/java/com/example/mediaapplication/MainActivity.java
+++ app/src/main/java/com/example/mediaapplication/MainActivity.java
@@ -6,13 +6,21 @@ import android.support.v7.widget.RecyclerView;
 
 import com.example.mediaapplication.api.ApiManager;
 import com.example.mediaapplication.api.ResponseListener;
+import com.example.mediaapplication.model.Picture;
 import com.example.mediaapplication.model.ServerResponse;
+import com.example.mediaapplication.model.User;
 import com.example.mediaapplication.recycler.RecyclerImpl;
+import com.example.mediaapplication.recycler.RecyclerItem;
+import com.example.mediaapplication.recycler_items.ImageItem;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
 
     private RecyclerImpl recyclerImpl;
+    List<RecyclerItem> list = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,18 +31,23 @@ public class MainActivity extends AppCompatActivity {
 
         recyclerImpl = new RecyclerImpl(recyclerView);
 
-//        ApiManager apiManager = new ApiManager();
-//        apiManager.getResults(new ResponseListener() {
-//            @Override
-//            public void successResponse(ServerResponse response) {
-//                response.getResults();
-//            }
-//
-//            @Override
-//            public void failureResponse() {
-//
-//            }
-//        });
+        ApiManager apiManager = new ApiManager();
+        apiManager.getResults(new ResponseListener() {
+            @Override
+            public void successResponse(ServerResponse response) {
+                List<User> users = response.getResults();
+                for (int i = 0; i < users.size(); i++) {
+                    Picture picture = users.get(i).getPicture();
+                    list.add(new ImageItem(picture));
+                }
+                recyclerImpl.updateItems(list);
+            }
+
+            @Override
+            public void failureResponse() {
+
+            }
+        });
 
 
     }
